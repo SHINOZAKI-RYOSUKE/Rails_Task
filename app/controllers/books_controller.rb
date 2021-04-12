b64586bb0cac59efb0bda321f6e5f114c0f46f95
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-04-12 13:09:02 +0000

    [update3]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,14 @@
 class BooksController < ApplicationController
+
   def index
+    @book_all = Book.all
+    @book_new = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -7,4 +16,17 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def destriy
+  end
+
+
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resouces:books
-  get 'homes/top'
+  
+  root to: 'homes#top'
+  
+  resources:books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
