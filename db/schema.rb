e51e407fd5499f03cb4c87c8342976f7cb9ce458
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-04-12 07:48:40 +0000

    [update2]

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_12_055830) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: falsehomes
+    t.datetime "updated_at", null: false
+  end
 
 end
